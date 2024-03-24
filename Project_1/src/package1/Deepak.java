d6c3a2b317d2efb1eac57437f86281ee5a4aa757
deepakchanana <deepakdeep1992@gmail.com>
2024-03-24 10:18:19 +0530

    gitbashcommit

--- Project_1/src/package1/Deepak.java
+++ Project_1/src/package1/Deepak.java
@@ -4,6 +4,7 @@ public class Deepak {
 	
 	public static void main(String[] args) {
 		System.out.println("changes done, ready for the commit");
+               System.out.println("changes done by deepak chanana");
 	}
 
 }
